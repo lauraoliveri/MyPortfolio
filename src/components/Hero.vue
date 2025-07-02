29e08affefab91869e5b02f73205d72cb648c940
lauraoliveri <lauraoliveri2003@gmail.com>
2025-07-02 12:55:37 +0200

    Add components files

---
+++ src/components/Hero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return { 
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>MIAOOOO</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+
+</style>
