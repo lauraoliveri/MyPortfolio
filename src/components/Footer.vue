a892543bda799d0299229b7d7b4ac3de5a497053
lauraoliveri <lauraoliveri2003@gmail.com>
2025-07-04 20:58:03 +0200

    Change of website design: update img folder, color palette in 'variables.css' and minor edits in Footer and Hero

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,11 +9,50 @@ export default {
 </script>
 
 <template>
-  
+  <footer>
+    <div class="text-center pt-2">
+        
+        <div class="fs-4 fw-bold yellow_bg">
+            &#8679;
+        </div>
+        <div  class="yellow_bg">
+            BACK TO TOP
+        </div>
+       
+        <div>
+            <p class="copyrights pt-3 pb-2">
+                © 2025 Laura Salvatrice Oliveri. All rights reserved. <br>
+                Design & development by me.  <br>
+                Icons by Lordicon, Flaticons, images by Freepik.
+
+            </p>
+        </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
+footer {
+    width: 100%;
+    height: 170px;
+    background-image: url(../assets/images/8169814.jpg);
+    background-position: center center;    
+    background-repeat: no-repeat;
+    background-size: cover; 
+    color: $mainWhite;
+}
 
+.copyrights {
+    color: black;
+    font-size: smaller;
+    opacity: 0.5;
+}
+
+.yellow_bg {
+    color: black;
+    background-color: $yellow;
+    
+}
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,13 +9,71 @@ export default {
 </script>
 
 <template>
-  <div>
-    <p>MIAOOOO</p>
+  <div class="bg_container">
+
+    
+
+    <!-- Navbar with links -->
+     
+    <nav id="content">
+        <div class="nav-container">
+            <div class="nav-links mx-5">
+                <ul class="d-flex me-5 justify-content-center">
+                    <li class="fs-5 fw-semibold"><a href="#">ABOUT</a></li>
+                    <li class="fs-5 fw-semibold"><a href="#">SKILLS</a></li>
+                    <li class="fs-5 fw-semibold"><a href="#">CONTACTS</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
   </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
+.bg_container {
+  background-image: url(../assets/images/8169822.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100vh; 
+  overflow: hidden;
+}
 
+.bg_container #content {
+  position: relative;
+  z-index: 1;
+  color: black;
+  text-align: center;
+  padding: 2rem;
+}
+
+
+.bg_container .navbar {
+  background-color: rgba(255, 255, 255, 0);
+}
+
+h1 {
+    font-family: 'Relashionship of Melodrame', sans-serif;
+}
+
+ul {
+    list-style: none;
+
+    li {
+
+        margin-right: 100px;
+        :hover {
+            color: rgb(0, 0, 0, 0.5);
+        }
+
+        a {
+            text-decoration: none;
+            color: $yellow;  
+        }
+    }
+     
+}
 </style>
