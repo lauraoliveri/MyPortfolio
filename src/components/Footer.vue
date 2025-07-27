80d991e8a0063937de7922af02e9f6392ffb5cc5
lauraoliveri <lauraoliveri2003@gmail.com>
2025-07-27 23:59:19 +0200

    Edit design and add sections

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,10 +12,12 @@ export default {
   <footer>
     <div class="text-center pt-2">
         
-        <div class="fs-4 fw-bold yellow_bg">
+        <div class="fs-4 fw-bold yellow_bg symbol">
+          <div>
             &#8679;
+          </div>
         </div>
-        <div  class="yellow_bg">
+        <div  class="yellow_bg back_to">
             BACK TO TOP
         </div>
        
@@ -35,24 +37,35 @@ export default {
 @use '../assets/scss/partials/variables' as *;
 
 footer {
-    width: 100%;
-    height: 170px;
-    background-image: url(../assets/images/8169814.jpg);
-    background-position: center center;    
-    background-repeat: no-repeat;
-    background-size: cover; 
-    color: $mainWhite;
+  width: 100%;
+  height: 180px;
+  background-image: url(../assets/images/8169814.jpg);
+  background-position: center center;    
+  background-repeat: no-repeat;
+  background-size: cover; 
+  color: $custom_white;
 }
 
 .copyrights {
-    color: black;
-    font-size: smaller;
-    opacity: 0.5;
+  color: black;
+  font-size: smaller;
+  opacity: 0.5;
+}
+
+.symbol {
+  width: 100px;
+  text-align: center;
 }
 
 .yellow_bg {
-    color: black;
-    background-color: $yellow;
-    
+  color: black;
+  background-color: $yellow;
+  padding: 1px;
+}
+
+
+
+.back_to{
+  width: 105px;
 }
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -10,15 +10,12 @@ export default {
 
 <template>
   <div class="bg_container">
-
-    
-
     <!-- Navbar with links -->
      
     <nav id="content">
         <div class="nav-container">
             <div class="nav-links mx-5">
-                <ul class="d-flex me-5 justify-content-center">
+                <ul class="d-flex  justify-content-center">
                     <li class="fs-5 fw-semibold"><a href="#">ABOUT</a></li>
                     <li class="fs-5 fw-semibold"><a href="#">SKILLS</a></li>
                     <li class="fs-5 fw-semibold"><a href="#">CONTACTS</a></li>
@@ -27,6 +24,51 @@ export default {
         </div>
     </nav>
 
+
+    <!-- About Me Section -->
+     <section class="d-flex justify-content-between text-center">
+      <div>
+        <!-- Text -->
+        <div>
+          <h4>
+            Hi!I'm
+          </h4>
+        </div>
+        <div>
+          <h1>
+            Laura
+          </h1>
+        </div>
+
+        <!-- Job -->
+        <div>
+          <h3>
+            Full-Stack Web Developer
+          </h3>   
+        </div>
+
+        <!-- Pic -->
+        <div id="my_pic">
+          <img src="./../assets/images/my-pic.jpg" alt="Laura Oliveri">
+        </div>
+      </div>
+      <div class="about_me_container">
+        <div>
+          <h2>
+            ABOUT ME
+          </h2>
+        </div>
+
+        <!-- paragraph about me -->
+         <p>
+          Ciao! Sono una ragazza di 22 anni, curiosa e determinata, sempre pronta ad affrontare nuove sfide e crescere.<br><br>
+          Dopo aver realizzato il mio sogno d’infanzia lavorando come hostess di volo, ho scoperto una nuova passione: l’informatica.<br>
+          Questo mi ha portato a frequentare un corso intensivo di 700 ore in <strong>sviluppo front-end e back-end</strong>, durante il quale ho acquisito competenze solide e tanta voglia di continuare ad imparare.<br><br>
+          Ora cerco un’opportunità per mettere in pratica ciò che ho appreso e continuare a crescere nel mondo dello sviluppo web.<br><br>
+          Nel tempo libero mi rilasso tra musica, chitarra, letture, videogame… e un numero forse eccessivo di video di gattini su Internet :).
+        </p>
+      </div>
+     </section>
   </div>
 </template>
 
@@ -55,10 +97,6 @@ export default {
   background-color: rgba(255, 255, 255, 0);
 }
 
-h1 {
-    font-family: 'Relashionship of Melodrame', sans-serif;
-}
-
 ul {
     list-style: none;
 
@@ -75,5 +113,52 @@ ul {
         }
     }
      
+}
+
+// About me section
+h1 {
+  font-family: 'Relashionship of Melodrame', sans-serif;
+  font-size: 8rem;
+  color: black;
+}
+
+h3 {
+  color: $pink;
+  text-decoration: underline;
+}
+
+#my_pic img {
+  width: 335px;
+  border-radius: 100%;
+}
+
+section {
+  padding-left: 150px;
+  padding-right: 150px;
+
+}
+.about_me_container {
+
+  padding-top: 100px;
+  width: 550px;
+  
+
+  h2{
+    color: black;
+    text-decoration: none;
+    font-family: 'Relashionship of Melodrame', sans-serif;
+
+    
+  }
+
+  p{
+    font-size: large;
+  }
+
+  &:hover{
+    h2 {
+      text-decoration: underline;
+    }
+  }
 }
 </style>
