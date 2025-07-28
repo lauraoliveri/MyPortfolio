98e203dfd8e7d4aa9a79bf8131653dee6e57cd47
lauraoliveri <lauraoliveri2003@gmail.com>
2025-07-28 23:25:32 +0200

    Minor changes

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -9,11 +9,26 @@ export default {
 </script>
 
 <template>
-  
+  <section>
+    <h2 class="py-5">
+      SKILLS
+    </h2>
+
+    <hr>
+  </section>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
+section {
+  background-color: $custom_white;
+  text-align: center;
+  height: 400px;
+}
+
+h2 {
+  font-family: 'Relashionship of Melodrame', sans-serif;
+}
 
 </style>
