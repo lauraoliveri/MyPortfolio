ae183fe64020158d1fd3b450e80e37d0eb6a7c1d
lauraoliveri <lauraoliveri2003@gmail.com>
2025-07-30 22:49:10 +0200

    Commence coding section

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -11,9 +11,42 @@ export default {
 <template>
   <section>
     <h2 class="py-5">
-      SKILLS
+      PROJECTS
     </h2>
 
+    <!-- Cards container -->
+     <div class="d-flex justify-content-evenly">
+      <div class="card_c  p-2">
+        <div class="img_square">
+          <img src="" alt="">
+        </div>
+        <div>
+          <h4>
+            PROGETTO 1
+          </h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam debitis aperiam itaque nobis consequuntur provident, iste eveniet eligendi porro voluptatem molestias inventore ipsam optio quo accusantium possimus officia. Doloremque, sequi.
+          </p>
+        </div>
+      </div>
+
+      <div class="card_c  p-2">
+        <div class="img_square">
+          <img src="" alt="">
+        </div>
+        <div>
+          <h4>
+            PROGETTO 1
+          </h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam debitis aperiam itaque nobis consequuntur provident, iste eveniet eligendi porro voluptatem molestias inventore ipsam optio quo accusantium possimus officia. Doloremque, sequi.
+          </p>
+        </div>
+      </div>
+      <div>
+
+      </div>
+     </div>
     <hr>
   </section>
 </template>
@@ -24,11 +57,22 @@ export default {
 section {
   background-color: $custom_white;
   text-align: center;
-  height: 400px;
 }
 
-h2 {
+h2, h4 {
   font-family: 'Relashionship of Melodrame', sans-serif;
 }
 
+.card_c {
+  width: 500px;
+  text-align: center;
+}
+
+.img_square {
+  background-color: $yellow;
+  width: 300px;
+  height: 300px;
+  text-align: center;
+  padding: 10px;
+}
 </style>
