6c12ec17d24acdb078fbd18cdef18d0b89c099a8
lauraoliveri <lauraoliveri2003@gmail.com>
2025-12-02 23:25:56 +0100

    Add css + change text in 'about' section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,8 @@
 <script>
 export default {
   data() {
-    return { 
-      
+    return {
+
     }
   }
 }
@@ -10,37 +10,47 @@ export default {
 
 <template>
   <section id="about">
-    <div class="container about_me">
+    <div class="container about_me py-5">
       <div class="row">
 
         <!-- left part: description -->
         <div class="col-md-6 col-12">
-         <div>
-            <h1>
-              ABOUT ME
+          <div class="d-flex align-items-center">
+            <div class="pe-2 ">
+              <img src="/src/assets/images/flower.svg" alt="">
+            </div>
+            <h1 class="text-uppercase">
+              ⎯ ABOUT ME
             </h1>
           </div>
 
           <!-- paragraph about me -->
           <p class=" pb-2">
-            Ciao! Sono una ragazza di 22 anni, curiosa e determinata, sempre pronta ad affrontare nuove sfide e crescere.<br><br>
-            Dopo aver realizzato il mio sogno d’infanzia lavorando come hostess di volo, ho scoperto una nuova passione: l’informatica.<br>
-            Questo mi ha portato a frequentare un corso intensivo di 700 ore in <strong>sviluppo front-end e back-end</strong>, durante il quale ho acquisito competenze solide e tanta voglia di continuare ad imparare.<br><br>
-            Ora cerco un’opportunità per mettere in pratica ciò che ho appreso e continuare a crescere nel mondo dello sviluppo web.<br><br>
-            Nel tempo libero mi rilasso tra musica, chitarra, letture, videogame… e un numero forse eccessivo di video di gattini su Internet :).
+            I’m a 22-year-old, curious and determined aspiring developer, always ready to learn and grow.<br><br>
+            After fulfilling my childhood dream of working as a flight attendant, I discovered a new passion: computer
+            science.<br>
+            This led me to complete an intensive 700-hour course in <strong>front-end</strong> and
+            <strong>back-end</strong> development,
+            where I gained solid skills and a strong desire to keep learning. I also completed a course in Java, further
+            expanding my programming skillset.<br><br>
+            <span class="text-decoration-underline">I’m now looking for my first professional opportunity in IT</span>,
+            excited to apply my skills, contribute to real projects, and continue growing as a web developer.<br>
+            In my free time, I enjoy music, playing guitar, reading, gaming… and watching too many adorable cat videos
+            online!:) <br><br>
           </p>
 
           <!-- resume button -->
           <div>
-            <button type="button" class="btn btn-outline">View Resume</button>
+            <button type="button" class="btn btn-outline text-uppercase">View Resume</button>
           </div>
         </div>
 
         <!-- right part with pic -->
         <div class="col-md-6 col-12 text-end">
-          <img src="/src/assets/images/me.png" alt="">
+          <img src="/src/assets/images/me.png" alt="" class="polaroid">
         </div>
       </div>
+
     </div>
   </section>
 </template>
@@ -48,34 +58,31 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
-// About me section
-
-section{
+section {
   background-color: $black;
   color: $white;
-
-
+  width: 100%;
+  min-height: 90vh;
 }
 
 
-img {
+.polaroid {
   width: 335px;
   padding-top: 20px;
   transform: rotate(5deg);
 }
 
 .about_me {
-  h1{
+  h1 {
     color: $pink;
     text-decoration: none;
     font-family: 'VT323', sans-serif;
   }
 
-  p{
+  p {
     font-size: large;
   }
 
-  @include underline-on-hover;
 }
 
 // Button
@@ -89,13 +96,18 @@ img {
   border: 2px solid $btn-outline-border;
   border-radius: $btn-outline-border-radius;
 
+  /* Neon shadow */
+  box-shadow:
+    0 0 8px rgba($pink, 0.6),
+    0 0 16px rgba($pink, 0.4),
+    0 0 24px rgba($white, 0.15);
+
   &:hover {
     background-color: $btn-outline-hover-bg;
     color: $btn-outline-hover-color;
     border-color: $btn-outline-hover-border;
- 
+
   }
 
 }
-
 </style>
