bad3491076b62631300d8d4ed2895df4c299d2cc
lauraoliveri <lauraoliveri2003@gmail.com>
2025-12-02 15:02:03 +0100

    Add section divider + css in 'Hero' component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -52,7 +52,14 @@ export default {
       </div>
     </div>
 
+            <!-- Section divider -->
+  <div class="custom-shape-divider-bottom-1764679236">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+    </svg>
+</div>
   </div>
+
 </template>
 
 <style lang="scss" scoped>
@@ -62,8 +69,32 @@ export default {
   color: $white;
   width: 100%;
   min-height: 100vh;
+  position: relative;
 }
 
+// Section divider 
+
+.custom-shape-divider-bottom-1764679236 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+}
+
+.custom-shape-divider-bottom-1764679236 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 18px;
+}
+
+.custom-shape-divider-bottom-1764679236 .shape-fill {
+    fill: #1D1313;
+}
+
+// 
 .navbar {
   background-color: $pink;
   text-align: center;
@@ -74,7 +105,6 @@ export default {
   width: 30px;
   border-top: 50px;
 }
-
 
 // .bg_container .navbar {
 //   background-color: rgba(255, 255, 255, 0);
